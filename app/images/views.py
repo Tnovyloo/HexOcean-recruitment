95ee8ac4047f98be4ae8de1cef728ca2cea8bee3
TnovyLoo <loobrome@gmail.com>
2023-02-23 15:46:29 +0100

    Fixing get serializer class method.

--- app/images/views.py
+++ app/images/views.py
@@ -7,7 +7,10 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from core.models import Image
-from images import serializers
+from .serializers import (ImageBasicUserSerializer,
+                        ImagePremiumUserSerializer
+)
+
 
 
 class ImageViewSet(mixins.ListModelMixin,
@@ -17,19 +20,27 @@ class ImageViewSet(mixins.ListModelMixin,
                         viewsets.GenericViewSet):
     """View set for Image model."""
     queryset = Image.objects.all()
-    serializer_class = serializers.ImageBasicUserSerializer
+    serializer_class = ImageBasicUserSerializer
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get_serializer_class(self):
         """Getting user membership and assign right serializer."""
         user = self.request.user
-        print(self.request.user)
-        print(self.request.user.membership)
-        if user.membership == "BASIC":
-            return serializers.ImageBasicUserSerializer
-        if user.membership == "PREMIUM":
-            return serializers.ImagePremiumUserSerializer
+        # print(self.request.user)
+        # print(self.request.user.membership)
+
+        # FOR OLD ONE IN MODELS CHOICES
+        # if user.membership == "BASIC":
+        #     return ImageBasicUserSerializer
+        # if user.membership == "PREMIUM":
+        #     return ImagePremiumUserSerializer
+
+        # FOR NEW ONE WHEN TIER IS A MODEL
+        if user.membership.tier_name == "BASIC":
+            return ImageBasicUserSerializer
+        if user.membership.tier_name == "PREMIUM":
+            return ImagePremiumUserSerializer
 
     def get_queryset(self):
         """Filtering queryset by user objects."""
